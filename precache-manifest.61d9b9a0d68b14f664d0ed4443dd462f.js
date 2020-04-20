9c9acbaeee84008cada7b1548e337f44d5fb1943
bannur <bannur.a@husky.neu.edu>
2020-04-20 10:44:45 -0400

    Updates

---
+++ precache-manifest.61d9b9a0d68b14f664d0ed4443dd462f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbe99400bc87e028887fdd77b0163b2",
+    "url": "/codebook-client/index.html"
+  },
+  {
+    "revision": "b82d39d54e05f027c5e5",
+    "url": "/codebook-client/static/css/main.58b52c17.chunk.css"
+  },
+  {
+    "revision": "a89b09d4353b9f1009dc",
+    "url": "/codebook-client/static/js/2.2c98b26e.chunk.js"
+  },
+  {
+    "revision": "2d22a8a8e956c6527362b4db28770241",
+    "url": "/codebook-client/static/js/2.2c98b26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82d39d54e05f027c5e5",
+    "url": "/codebook-client/static/js/main.366890f8.chunk.js"
+  },
+  {
+    "revision": "6f67b8a6e41ef5d30386",
+    "url": "/codebook-client/static/js/runtime-main.dd6be04a.js"
+  },
+  {
+    "revision": "ddd0118e42e43595474ddf8c1475f033",
+    "url": "/codebook-client/static/media/landing-page-image.ddd0118e.jpg"
+  },
+  {
+    "revision": "f52822f00b1d54f11b3f83963fe18743",
+    "url": "/codebook-client/static/media/person-holding-midnight-black-samsung-galaxy-s8-turn-on-near-1092671.f52822f0.jpg"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/codebook-client/static/media/showcase.4b31330b.jpg"
+  }
+]);
